8d5984f50ee05de7d06f75a2217c6484fe215e25
ayan-ali <ayan31990@gmail.com>
2026-01-14 10:32:51 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
 import React from "react";
-import './../styles/App.css';
+import Tooltip from "./Tooltip";
+import "./../styles/App.css";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <Tooltip text="This is a tooltip for heading">
+        <h2>Hover over this heading</h2>
+      </Tooltip>
+
+      <Tooltip text="This is a tooltip for paragraph">
+        <p>Hover over this paragraph</p>
+      </Tooltip>
+
+      <Tooltip text="This is a tooltip for button">
+        <button>Hover over this button</button>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Tooltip.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Tooltip = ({ text, children }) => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
+      {children}
+      {show && <span className="tooltiptext">{text}</span>}
+    </div>
+  );
+};
+
+export default Tooltip;
